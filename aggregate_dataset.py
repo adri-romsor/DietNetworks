204dbcc47e7661a056d88b16209d70e25de24543
carriepl <carrier.pierreluc@gmail.com>
2016-04-13 09:51:32 -0400

    Make aggregate_dataset.py work

--- aggregate_dataset.py
+++ aggregate_dataset.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import pandas as pd
+import gc
 
 
 def list_files (data_dir):
@@ -42,7 +43,7 @@ def list_files (data_dir):
 
 def parse_illumina(filename):
     l = []
-    with open(filename,"r",encoding="utf-8") as input_file:
+    with open(filename,"r") as input_file:
         for line in input_file:
             temp = line.split(",")
             temp = list(map(lambda l: l.strip('"\n'),temp))
@@ -53,24 +54,50 @@ def parse_illumina(filename):
 
 def parse_23_and_me(filename):
     l = []
-    with open(filename,"r",encoding="utf-8",errors='ignore') as input_file:
+    #with open(filename,"r",encoding="utf-8",errors='ignore') as input_file:
+    with open(filename,"r") as input_file:
         for line in input_file:
             if not line.startswith("#"):
                 temp = line.split("\t")
-                temp = list(map(lambda l: l.strip('"\n'),temp))
+                temp = list(map(lambda l: l.strip(),temp))
                 #if not (len(temp) == 4):
                 #    print (temp)
                 assert (len(temp) == 4)
-                l.append(temp)
+                l.append([temp[0], temp[3]])
     return l
 
 def parse_list (l):
     d = {}
     for i in l:
         if i[0] in d:
-            assert d[i[0]] == d[3]
+            assert d[i[0]] == d[1]
         else:
-            d[i[0]] = i[3]
+            d[i[0]] = i[1]
+    return d
+
+
+def add_dicts_validate(dict1, dict2):
+    for k in dict2.keys():
+        if k in dict1:
+            assert dict1[k] == dict2[k]
+        else:
+            dict1[k] = dict2[k]
+
+
+def parse_23_and_me_dict(filename, feature_values_dict):
+    d = {}
+    #with open(filename,"r",encoding="utf-8",errors='ignore') as input_file:
+    with open(filename,"r") as input_file:
+        for line in input_file:
+            if not line.startswith("#"):
+                temp = line.split("\t")
+                temp = list(map(lambda l: l.strip(),temp))
+                assert (len(temp) == 4)
+                
+                if temp[0] in d:
+                    assert d[temp[0]] == feature_values_dict[temp[3]]
+                else:
+                    d[temp[0]] = feature_values_dict[temp[3]]
     return d
 
 
@@ -83,28 +110,63 @@ if __name__ == "__main__":
     #test_file = os.path.join(data_dir,files["ftdna-illumina"][0])
     #print (test_file)
     test_file = os.path.join(data_dir,files["23_and_me"][0])
-    #print (len(parse_23_and_me(test_file)))
     #print (parse_illumina(test_file))
     print ("Parsing 23 and me data")
-
+    
+    
+    feature_values_dict = {'A' : 1, 'AA' : 2, 'AC' : 3, 'AG' : 4, 'AT' : 5,
+                           'C' : 6, 'CC' : 7, 'CG' : 8, 'CT' : 9, 'D' : 10,
+                           'DD' : 11, 'DI' : 12, 'G' : 13, 'GG' : 14,
+                           'GT' : 15, 'I' : 16, 'II' : 17, 'T' : 18,
+                           'TT' : 19, '--' : 0}
+    
     data23andme = {}
 
-    for filename in files["23_and_me"][:2]:
+    for index, filename in enumerate(files["23_and_me"]):
+        print("Processing %i out of %i" % (index, len(files["23_and_me"])))
         input_file = os.path.join(data_dir,filename)
         user_id = filename.split("_")[0]
-        temp = parse_23_and_me(input_file)
-        print (user_id)
-        if user_id in data23andme:
-            data23andme[user_id] += temp
-        else:
-            data23andme[user_id] = temp
+        try:
+            temp_dict = parse_23_and_me_dict(input_file, feature_values_dict)
+            
+            if user_id in data23andme:
+                add_dicts_validate(data23andme[user_id], temp_dict)
+            else:
+                data23andme[user_id] = temp_dict
+            
+        except:
+            print("Skipping user", user_id)
+        gc.collect()
+        
 
-    #for i in data23andme.keys():
-    #    data23andme[i] = np.array(data23andme[i])
-    #print (data23andme)
-    for i in data23andme.keys():
-        data23andme[i] = parse_list(data23andme[i])
-
-    #print (data23andme)
-    print ("23 and me")
-    print (pd.DataFrame.from_dict(data23andme,'index'))
+    # Convert the result to a numpy array
+    #####################################
+    
+    # Step 1 : create a set of all the feature ids and a dictionary mapping
+    # feature ids to numbers
+    feature_set = set()
+    for d in data23andme.values():
+        feature_set = feature_set.union(d.keys())
+    
+    feature_dict = {}
+    for i, feature_id in enumerate(feature_set):
+        feature_dict[feature_id] = i
+        
+    # Step 2 : allocate a numpy array large enough to contain the results
+    shape = (len(data23andme.keys()), len(feature_dict.keys()))
+    arr = np.zeros(shape, "int32")
+    
+    # Step 3 : go over the dict of dicts and insert the data in the allocated
+    # array.
+    for user_idx, user_dict in enumerate(data23andme.values()):
+        if user_idx % 10 == 0:
+            print user_idx
+        for feature_id in user_dict.keys():
+            
+            feature_value = user_dict[feature_id]
+            feature_idx = feature_dict[feature_id]
+            
+            arr[user_idx, feature_idx] = feature_value
+    
+    # Save the result to disk
+    np.save("/data/lisatmp4/carriepl/ma_dataset.npy", arr)
