0098569646da4651aac3829488d1dd86ee8349ad
Etienne Dejoie <etdej@github.com>
2016-05-02 16:13:36 -0400

    pimp kmeans and pca

--- experiments/baselines/kmeans.py
+++ experiments/baselines/kmeans.py
@@ -1,14 +1,12 @@
-import sys
-sys.path.append('/data/lisatmp4/dejoieti/')
-
 from sklearn.cluster import KMeans
-from feature_selection.experiments.common.dorothea import load_data
+from experiments.common.dorothea import load_data
 
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def kmeans_dorothea(n_cl=50):
+def kmeans_dorothea(n_cl, save_path):
     save_path = '/data/lisatmp4/dejoieti/feature_selection/'
 
     x_train, y_train = load_data('train', return_format='numpy')
@@ -20,27 +18,39 @@ def kmeans_dorothea(n_cl=50):
 
     file_name = save_path + 'kmeans_' + str(n_cl) + '_embedding.npz'
     np.savez(file_name, x_train=new_x_train, y_train=y_train,
-                        x_valid=new_x_valid, y_valid=y_valid)
+             x_valid=new_x_valid, y_valid=y_valid)
 
     return new_x_train, km
 
 
 if __name__ == '__main__':
-    x_train, y_train = load_data('train', return_format='numpy')
-    new_x, kmeans = kmeans_dorothea(n_cl=50)
+    parser = argparse.ArgumentParser(description="""Kmeans embedding""")
+    parser.add_argument('-nc',
+                        type=int,
+                        default=50,
+                        help='number of components for embedding')
 
+    parser.add_argument('-save_path',
+                        '-sp',
+                        default='/data/lisatmp4/dejoieti/feature_selection/',
+                        help='number of components for embedding')
+
+    args = parser.parse_args()
+    
+    new_x, kmeans = kmeans_dorothea(args.nc, args.save_path)
 
     # The following is testig how accurate are the clusters to guess y
 
-    x_valid, y_valid = load_data('valid', return_format='numpy')
-    train_x_predict = kmeans.predict(x_train)
-    valid_x_predict = kmeans.predict(x_valid)
-
-    y_predict = [np.mean(y_train[train_x_predict == i])
-                 for i in valid_x_predict]
-
-    print(np.mean((y_predict - y_valid)**2))
-
-    y_predict_hard = [0 if y < 0.5 else 1 for y in y_predict]
-
-    print(np.mean((y_predict_hard - y_valid)**2))
+    # x_train, y_train = load_data('train', return_format='numpy')
+    # x_valid, y_valid = load_data('valid', return_format='numpy')
+    # train_x_predict = kmeans.predict(x_train)
+    # valid_x_predict = kmeans.predict(x_valid)
+    #
+    # y_predict = [np.mean(y_train[train_x_predict == i])
+    #              for i in valid_x_predict]
+    #
+    # print(np.mean((y_predict - y_valid)**2))
+    #
+    # y_predict_hard = [0 if y < 0.5 else 1 for y in y_predict]
+    #
+    # print(np.mean((y_predict_hard - y_valid)**2))
--- experiments/baselines/pca.py
+++ experiments/baselines/pca.py
@@ -1,16 +1,10 @@
-import sys
-sys.path.append('/data/lisatmp4/dejoieti/')
-
 from sklearn.decomposition import PCA
-from feature_selection.experiments.common.dorothea import load_data
-from feature_selection.config import path_dorothea
+from experiments.common.dorothea import load_data
 
+import argparse
 import numpy as np
-import matplotlib.pyplot as plt
 
-def pca_dorothea(n_comp=50):
-    save_path = '/data/lisatmp4/dejoieti/feature_selection/'
-
+def pca_dorothea(n_comp, save_path):
     x_train, y_train = load_data('train', return_format='numpy')
     x_valid, y_valid = load_data('valid', return_format='numpy')
     pca = PCA(n_components=n_comp)
@@ -20,14 +14,24 @@ def pca_dorothea(n_comp=50):
 
     file_name = save_path + 'pca_' + str(n_comp) + '_embedding.npz'
     np.savez(file_name, x_train=new_x_train, y_train=y_train,
-                        x_valid=new_x_valid, y_valid=y_valid)
+             x_valid=new_x_valid, y_valid=y_valid)
 
     return pca
 
 
 if __name__ == '__main__':
-    pca_dorothea(n_comp=100)
+    parser = argparse.ArgumentParser(description="""PCA embedding""")
+    parser.add_argument('-nc',
+                        type=int,
+                        default=100,
+                        help='number of components for embedding')
 
+    parser.add_argument('-save_path',
+                        '-sp',
+                        default='/data/lisatmp4/dejoieti/feature_selection/',
+                        help='number of components for embedding')
 
+    args = parser.parse_args()
 
+    pca_dorothea(args.nc, args.save_path)
     # plt.plot(np.cumsum(pca.explained_variance_ratio_))
