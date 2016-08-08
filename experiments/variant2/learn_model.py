e2323391cc22ec72ee8e5a1b729d2598aa16b000
erakra <erraqabi@gmail.com>
2016-08-08 10:50:08 -0400

    learn_model merged

--- experiments/variant2/learn_model.py
+++ experiments/variant2/learn_model.py
@@ -26,18 +26,6 @@ def iterate_minibatches(inputs, targets, batchsize,
         yield inputs[indices[i:i+batchsize], :],\
             targets[indices[i:i+batchsize]]
 
-    # if shuffle:
-    #     indices = np.arange(len(inputs))
-    #     np.random.shuffle(indices)
-    #
-    # for start_idx in range(0, len(inputs) - batchsize + 1, batchsize):
-    #     if shuffle:
-    #         excerpt = indices[start_idx:start_idx + batchsize]
-    #     else:
-    #         excerpt = slice(start_idx, start_idx + batchsize)
-    #
-    # yield inputs[excerpt], targets[excerpt]
-
 
 def iterate_testbatches(inputs, batchsize, shuffle=False):
     indices = np.arange(inputs.shape[0])
