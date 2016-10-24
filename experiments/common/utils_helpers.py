048fdb6266a76880c76d19576ca081e3ea96573f
Adriana Romero <adriana.romsor@gmail.com>
2016-10-24 17:20:36 -0400

    adding hist functions

--- experiments/common/utils_helpers.py
+++ experiments/common/utils_helpers.py
@@ -1,10 +1,11 @@
+import os
 import numpy as np
 import dataset_utils as du
 
 
-def load_1000_genomes_hist(transpose=False, label_splits=None,
-                           feature_splits=None, fold=0, perclass=False,
-                           norm=True):
+def generate_1000_genomes_hist(transpose=False, label_splits=None,
+                               feature_splits=None, fold=0, perclass=False,
+                               norm=True):
 
     train, valid, test, _ = du.load_1000_genomes(transpose, label_splits,
                                                  feature_splits, fold,
@@ -16,33 +17,32 @@ def load_1000_genomes_hist(transpose=False, label_splits=None,
 
     nolabel_y = nolabel_y.argmax(axis=1)
 
+    path = '/data/lisatmp4/romerosa/datasets/1000_Genome_project/'
+
+    filename = 'unsupervised_hist_3x26.npy' if perclass else \
+        'unsupervised_hist_3.npy'
+
     if perclass:
         nolabel_x = np.zeros((nolabel_orig.shape[0], 3*26))
         for i in range(nolabel_x.shape[0]):
             for j in range(nolabel_x.shape[1]):
                 nolabel_x[i, nolabel_y[j]*3:nolabel_y[j]*3+3] += \
-                    np.bincount(nolabel_orig[i, :].astype('int32'), minlength=3)
+                    np.bincount(nolabel_orig[i, :].astype('int32'),
+                                minlength=3)
     else:
         nolabel_x = np.zeros((nolabel_orig.shape[0], 3))
         for i in range(nolabel_x.shape[0]):
             nolabel_x[i, :] += np.bincount(nolabel_orig[i, :].astype('int32'),
                                            minlength=3)
 
-    if norm:
-        train[0] = (train[0] - train[0].mean(axis=0)[None, :]) / \
-            train[0].std(axis=0)[None, :]
-        valid[0] = (valid[0] - valid[0].mean(axis=0)[None, :]) / \
-            valid[0].std(axis=0)[None, :]
-        test[0] = (test[0] - test[0].mean(axis=0)[None, :]) / \
-            test[0].std(axis=0)[None, :]
-
     nolabel_x = nolabel_x.astype('float32')
 
-    import ipdb; ipdb.set_trace()
+    np.save(os.path.join(path, filename), nolabel_x)
 
-    return train, valid, test, nolabel_x
+    return
+
 
 if __name__ == '__main__':
-    train, valid, test, nolabel_x = load_1000_genomes_hist(
+    train, valid, test, nolabel_x = generate_1000_genomes_hist(
         transpose=False, label_splits=[.75], feature_splits=None, fold=0,
-        perclass=True, norm=True)
+        perclass=False, norm=True)
