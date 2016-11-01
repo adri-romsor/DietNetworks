6663588c3514e0cc831e68d50e034cf4bb3732c9
Adriana Romero <adriana.romsor@gmail.com>
2016-11-01 11:22:25 -0400

    normalization train+valid and relu

--- experiments/baselines/pca.py
+++ experiments/baselines/pca.py
@@ -1,28 +1,25 @@
 from sklearn.decomposition import RandomizedPCA, TruncatedSVD, PCA
 from aggregate_dataset import load_data23andme_baselines
 from feature_selection.experiments.common import dataset_utils, imdb
+from feature_selection.experiments.variant2 import mainloop_helpers as mlh
 
 import argparse
 import os
 import numpy as np
 import time
 
-def pca(dataset, n_comp_list, save_path, method="pca"):
+def pca(dataset, n_comp_list, save_path, method="pca", which_fold=0):
 
     # Load the dataset
     print("Loading data")
-    splits = [0.6, 0.2]  # This will split the data into [60%, 20%, 20%]
+    x_train, y_train, x_valid, y_valid, x_test, y_test, \
+        _, _ = mlh.load_data(
+            dataset, dataset_path, embedding_source,
+            which_fold=which_fold, keep_labels=keep_labels,
+            missing_labels_val=missing_labels_val,
+            embedding_input=embedding_input, norm=False)
 
-    if dataset == "opensnp":
-        x_train, x_test, unsupervised = load_data23andme_baselines()
-    elif dataset == '1000_genomes':
-        data = dataset_utils.load_1000_genomes(transpose=False,
-                                               label_splits=splits)
-        (x_train, y_train), (x_valid, y_valid), (x_test, y_test),\
-            x_nolabel = data
-        unsupervised = np.concatenate((x_train, x_valid), axis=0)
-    else:
-        raise ValueError("Unknown dataset")
+    unsupervised = np.concatenate((x_train, x_valid), axis=0)
 
     save_path = os.path.join(save_path, dataset)
     if not os.path.exists(save_path):
@@ -35,12 +32,8 @@ def pca(dataset, n_comp_list, save_path, method="pca"):
         pca.fit(unsupervised)
         # Apply PCA to supervised training data
         new_x_train_supervised = pca.transform(x_train)
+        new_x_valid_supervised = pca.transform(x_valid)
         new_x_test_supervised = pca.transform(x_test)
-
-        if dataset != 'opensnp':
-            new_x_valid_supervised = pca.transform(x_valid)
-        else:
-            new_x_valid_supervised = None
 
         # Remove items from n_comp_list that are outside the bounds
         max_n_comp = new_x_train_supervised.shape[1]
@@ -49,7 +42,7 @@ def pca(dataset, n_comp_list, save_path, method="pca"):
 
         print "Saving embeddings"
         for n_comp in n_comp_list:
-            file_name = save_path + '/pca_' + str(n_comp) + '_embedding.npz'
+            file_name = save_path + '/pca_' + str(n_comp) + '_embedding_fold' + str(which_fold) + '.npz'
             np.savez(file_name,
                      x_train_supervised=new_x_train_supervised[:, :n_comp],
                      y_train_supervised=y_train,
@@ -57,61 +50,6 @@ def pca(dataset, n_comp_list, save_path, method="pca"):
                      y_valid_supervised=y_valid,
                      x_test_supervised=new_x_test_supervised[:, :n_comp],
                      y_test_supervised=y_test)
-
-    elif method == "randPCA":
-        print "Applying PCA"
-        # Extract PCA from unsupervised data
-        pca = RandomizedPCA()
-        pca.fit(unsupervised)
-        # Apply PCA to supervised training data
-        new_x_train_supervised = pca.transform(x_train)
-        new_x_test_supervised = pca.transform(x_test)
-
-        if dataset != 'opensnp':
-            new_x_valid_supervised = pca.transform(x_valid)
-        else:
-            new_x_valid_supervised = None
-
-        # Remove items from n_comp_list that are outside the bounds
-        max_n_comp = new_x_train_supervised.shape[1]
-        n_comp_possible = [el for el in n_comp_list if el < max_n_comp]
-        n_comp_list = n_comp_possible + [max_n_comp]
-
-        print "Saving embeddings"
-        for n_comp in n_comp_list:
-            file_name = save_path + '/rpca_' + str(n_comp) + '_embedding.npz'
-            np.savez(file_name,
-                     x_train_supervised=new_x_train_supervised[:, :n_comp],
-                     y_train_supervised=y_train,
-                     x_valid_supervised=new_x_valid_supervised[:, :n_comp],
-                     y_valid_supervised=y_valid,
-                     x_test_supervised=new_x_test_supervised[:, :n_comp],
-                     y_test_supervised=y_test)
-
-    elif method == "truncSVD":
-        # Apply truncated svd (pca) for each nber of clusters
-        for n_cl in n_comp_list:
-            start_time = time.time()
-            print "tsvd %d of out %d" % (n_cl, len(n_comp_list))
-            tsvd = TruncatedSVD(n_components=n_cl)
-            tsvd.fit(unsupervised)
-            new_x_train_supervised = tsvd.transform(x_train)
-            new_x_test_supervised = tsvd.transform(x_test)
-
-            if dataset != 'opensnp':
-                new_x_valid_supervised = tsvd.transform(x_valid)
-            else:
-                new_x_valid_supervised = None
-
-            # Save embeddings
-            file_name = save_path + '/tsvd_' + str(n_cl) + '_embedding.npz'
-            np.savez(file_name, x_train_supervised=new_x_train_supervised,
-                     y_train_supervised=y_train,
-                     x_valid_supervised=new_x_valid_supervised[:, :n_comp],
-                     y_valid_supervised=y_valid,
-                     x_test_supervised=new_x_test_supervised,
-                     y_test_supervised=y_test)
-            print "... took %f s" % (time.time() - start_time)
 
 
 if __name__ == '__main__':
@@ -123,13 +61,12 @@ if __name__ == '__main__':
                         help='dataset')
     parser.add_argument('-save_path',
                         '-sp',
-                        default='/data/lisatmp4/romerosa/feature_selection/',
+                        default='/data/lisatmp4/romerosa/feature_selection/pca_final/',
                         help='number of components for embedding')
 
     args = parser.parse_args()
 
-    pca(args.d, [1, 2, 5, 10, 20, 50,
-                 100, 200, 400, 600, 800,
-                 1000, 1200, 1400, 1600, 2000, 2400, 2600, 3000],
-        args.save_path)
+    for f in range(5):
+        pca(args.d, [10, 50, 100],
+            args.save_path, which_fold=f)
     # plt.plot(np.cumsum(pca.explained_variance_ratio_))
--- experiments/common/dataset_utils.py
+++ experiments/common/dataset_utils.py
@@ -207,11 +207,6 @@ def load_1000_genomes(transpose=False, label_splits=None, feature_splits=None,
         x, y = thousand_genomes.load_data(path)
         x = x.astype("float32")
 
-        mu_all = x.mean(axis=0)
-        sigma_all = x.std(axis=0)
-        print('Mean:' + str(mu_all.min()) + ' ' + str(mu_all.max()))
-        print('Std:' + str(sigma_all.min()) + ' ' + str(sigma_all.max()))
-
         (x, y) = shuffle((x, y))
 
         # Prepare training and validation sets
@@ -232,16 +227,18 @@ def load_1000_genomes(transpose=False, label_splits=None, feature_splits=None,
     if not transpose:
         train, valid = split([x, y], label_splits)
         if norm:
-            mu = train[0].mean(axis=0)
-            sigma = train[0].std(axis=0)
-            print('Mean:' + str(mu.min()) + ' ' + str(mu.max()))
-            print('Std:' + str(sigma.min()) + ' ' + str(sigma.max()))
+            mu = x.mean(axis=0)
+            sigma = x.std(axis=0)
+            # mu = train[0].mean(axis=0)
+            # sigma = train[0].std(axis=0)
+            # print('Mean:' + str(mu.min()) + ' ' + str(mu.max()))
+            # print('Std:' + str(sigma.min()) + ' ' + str(sigma.max()))
             train[0] = (train[0] - mu[None, :]) / sigma[None, :]
             valid[0] = (valid[0] - mu[None, :]) / sigma[None, :]
             test[0] = (test[0] - mu[None, :]) / sigma[None, :]
-            print('Min train: ' +str(train[0].min()) + ' max: ' +str(train[0].max()))
-            print('Min valid: ' +str(valid[0].min()) + ' max: ' +str(valid[0].max()))
-            print('Min test: ' +str(test[0].min()) + ' max: ' +str(test[0].max()))
+            # print('Min train: ' +str(train[0].min()) + ' max: ' +str(train[0].max()))
+            # print('Min valid: ' +str(valid[0].min()) + ' max: ' +str(valid[0].max()))
+            # print('Min test: ' +str(test[0].min()) + ' max: ' +str(test[0].max()))
         rvals = [train, valid, test]
     else:
         rvals = []
