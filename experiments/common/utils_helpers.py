50ed70b0dfa82e8f016f1faa9d0671432760a755
Adriana Romero <adriana.romsor@gmail.com>
2017-03-02 12:39:17 -0500

    last fix

--- experiments/common/utils_helpers.py
+++ experiments/common/utils_helpers.py
@@ -4,16 +4,19 @@ import dataset_utils as du
 
 
 def generate_1000_genomes_hist(transpose=False, label_splits=None,
-                               feature_splits=None, fold=0, perclass=False):
+                               feature_splits=None, fold=0, perclass=False,
+                               path = '/data/lisatmp4/romerosa/datasets/1000_Genome_project/'):
 
     """
     train, valid, test, _ = du.load_1000_genomes(transpose, label_splits,
                                                  feature_splits, fold,
                                                  norm=False)
     """
-    train, valid, test, _ = du.load_1000_genomes_old(transpose, label_splits,
-                                                     feature_splits, fold,
-                                                     norm=False)
+    train, valid, test, _ = du.load_1000_genomes(transpose=transpose,
+                                                 label_splits=label_splits,
+                                                 feature_splits=feature_splits,
+                                                 fold=fold,
+                                                 norm=False, nolabels='raw')
 
     # Generate no_label: fuse train and valid sets
     nolabel_orig = (np.vstack([train[0], valid[0]])).transpose()
@@ -21,21 +24,26 @@ def generate_1000_genomes_hist(transpose=False, label_splits=None,
 
     nolabel_y = nolabel_y.argmax(axis=1)
 
-    path = '/data/lisatmp4/romerosa/datasets/1000_Genome_project/'
-
-    filename = 'unsupervised_hist_3x26' if perclass else \
-        'unsupervised_hist_3'
+    filename = 'histo3x26' if perclass else \
+        'histo3'
     filename += '_fold' + str(fold) + '.npy'
 
     if perclass:
+        # the first dimension of the following is length 'number of snps'
         nolabel_x = np.zeros((nolabel_orig.shape[0], 3*26))
         for i in range(nolabel_x.shape[0]):
-            for j in range(nolabel_x.shape[1]):
-                nolabel_x[i, nolabel_y[j]*3:nolabel_y[j]*3+3] += \
-                    np.bincount(nolabel_orig[i, :].astype('int32'),
-                                minlength=3)
-                nolabel_x[i, nolabel_y[j]*3:nolabel_y[j]*3+3] /= \
-                    nolabel_x[i, nolabel_y[j]*3:nolabel_y[j]*3+3].sum()
+            if i % 5000 == 0:
+                print "processing snp no: ", i
+            for j in range(26):
+                nolabel_x[i, j*3:j*3+3] += \
+                    np.bincount(nolabel_orig[i, nolabel_y == j ].astype('int32'), minlength=3)
+                nolabel_x[i, j*3:j*3+3] /= \
+                    nolabel_x[i, j*3:j*3+3].sum()
+            # print nolabel_orig[0,:].shape
+            # print nolabel_orig[0,:].sum()
+            # print nolabel_y
+            # print zip(np.sum(nolabel_x[0,:].reshape(26,3), axis=1), np.bincount(nolabel_y.astype('int32')))
+            # print nolabel_x[0,:].reshape(26,3)
     else:
         nolabel_x = np.zeros((nolabel_orig.shape[0], 3))
         for i in range(nolabel_x.shape[0]):
@@ -50,7 +58,8 @@ def generate_1000_genomes_hist(transpose=False, label_splits=None,
 
 def generate_1000_genomes_bag_of_genes(
         transpose=False, label_splits=None,
-        feature_splits=[0.8], fold=0):
+        feature_splits=[0.8], fold=0,
+        path = '/data/lisatmp4/romerosa/datasets/1000_Genome_project/'):
 
     train, valid, test, _ = du.load_1000_genomes(transpose, label_splits,
                                                  feature_splits, fold,
@@ -58,7 +67,6 @@ def generate_1000_genomes_bag_of_genes(
 
     nolabel_orig = (np.vstack([train[0], valid[0]]))
 
-    path = '/data/lisatmp4/sylvaint/datasets/1000_Genome_project/'
     if not os.path.isdir(path):
         os.makedirs(path)
 
@@ -80,7 +88,8 @@ def generate_1000_genomes_bag_of_genes(
 
 
 def generate_1000_genomes_snp2bin(transpose=False, label_splits=None,
-                                  feature_splits=None, fold=0):
+                                  feature_splits=None, fold=0,
+                                  path = '/data/lisatmp4/romerosa/datasets/1000_Genome_project/'):
 
     train, valid, test, _ = du.load_1000_genomes_old(transpose, label_splits,
                                                      feature_splits, fold,
@@ -91,7 +100,7 @@ def generate_1000_genomes_snp2bin(transpose=False, label_splits=None,
     nolabel_x = np.zeros((nolabel_orig.shape[0], nolabel_orig.shape[1]*2),
                          dtype='uint8')
 
-    path = '/data/lisatmp4/romerosa/datasets/1000_Genome_project/'
+
     filename = 'unsupervised_snp_bin_fold' + str(fold) + '.npy'
 
     # SNP to bin
